a2e46bea5e55c64e246f347012dded971f73e714
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-04-28 01:04:10 +0200

    Setup of API keys

---
+++ scripts/01_Spotify_playlists.R
@@ -0,0 +1,16 @@
+library(spotifyr)
+library(httr)
+library(jsonlite)
+library(lubridate)
+library(knitr)
+library(tidyverse)
+
+client_id <- rstudioapi::askForPassword(prompt = "Please enter your client ID")
+client_secret <- rstudioapi::askForPassword(prompt = "Please enter your client secret")
+authorization_header <- NULL
+redirect_uri <- "http://localhost:1410/"
+
+Sys.setenv(SPOTIFY_CLIENT_ID = client_id)
+Sys.setenv(SPOTIFY_CLIENT_SECRET = client_secret)
+
+access_token <- get_spotify_access_token()
