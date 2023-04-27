cd0c1e128434609c691daa8e2a3e553b5065c419
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-04-28 01:19:03 +0200

    Remove for  innter scraping

--- scripts/03_Directions_API.R
+++ scripts/03_Directions_API.R
@@ -4,3 +4,11 @@ library(jsonlite)
 # Set up Google API key and endpoint URL
 api_key <- rstudioapi::askForPassword()
 Sys.setenv(GU_API_KEY = api_key) 
+
+
+# Define API endpoint and API key
+api_endpoint <- "https://maps.googleapis.com/maps/api/directions/json"
+#api_key <- "YOUR_API_KEY"
+
+# Define the start location
+start_location <- "University of Lucerne, Switzerland"
