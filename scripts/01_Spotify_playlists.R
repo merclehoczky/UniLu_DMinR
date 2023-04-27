9ac0f918190ca074685008c45bf4eb79dd0f08c4
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-04-28 01:05:37 +0200

    ID prompt

--- scripts/01_Spotify_playlists.R
+++ scripts/01_Spotify_playlists.R
@@ -5,6 +5,7 @@ library(lubridate)
 library(knitr)
 library(tidyverse)
 
+# Ask for client_id and client_secret
 client_id <- rstudioapi::askForPassword(prompt = "Please enter your client ID")
 client_secret <- rstudioapi::askForPassword(prompt = "Please enter your client secret")
 authorization_header <- NULL
@@ -14,3 +15,8 @@ Sys.setenv(SPOTIFY_CLIENT_ID = client_id)
 Sys.setenv(SPOTIFY_CLIENT_SECRET = client_secret)
 
 access_token <- get_spotify_access_token()
+
+# Ask for user id
+my_id <- rstudioapi::askForPassword(prompt = "Please enter your user ID")
+
+
