5f3f82a2bc8a19726618709cf0260c6fadeba29f
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-04-28 01:21:24 +0200

    Roads in ms

--- scripts/03_Directions_API.R
+++ scripts/03_Directions_API.R
@@ -18,3 +18,32 @@ restaurant_locations <- places$place_address
 
 # Create an empty list to store the route times
 route_times <- list()
+
+# Loop through each restaurant location and make a request to the Directions API
+for (i in seq_along(restaurant_locations)) {
+  # Define the destination
+  destination <- restaurant_locations[i]
+  
+  # Build the API query URL
+  query_url <- paste0(api_endpoint,
+                      "?origin=", URLencode(start_location),
+                      "&destination=", URLencode(destination),
+                      "&mode=walking",
+                      "&key=", api_key)
+  
+  # Make the API request
+  api_response <- GET(query_url)
+  
+  # Convert the response to JSON
+  response_json <- fromJSON(content(api_response, "text"), simplifyVector = FALSE)
+  
+  # Extract the route time in seconds and add it to the list
+  route_time <- response_json$routes[[1]]$legs[[1]]$duration$value
+  route_times[[i]] <- route_time
+}
+
+# Save the route times for each restaurant
+for (i in seq_along(route_times)) {
+  places$route_times_sec[i] <- as.numeric(route_times[[i]])
+}
+
