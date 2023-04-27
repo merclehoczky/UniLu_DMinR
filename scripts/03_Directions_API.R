14ed5fd00747ecafecbcfec007a3fb1af1b83a7b
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-04-28 01:20:08 +0200

    Restaurant location empty variable

--- scripts/03_Directions_API.R
+++ scripts/03_Directions_API.R
@@ -12,3 +12,9 @@ api_endpoint <- "https://maps.googleapis.com/maps/api/directions/json"
 
 # Define the start location
 start_location <- "University of Lucerne, Switzerland"
+
+# Define a list of restaurant locations
+restaurant_locations <- places$place_address
+
+# Create an empty list to store the route times
+route_times <- list()
