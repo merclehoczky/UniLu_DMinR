627a04b47ac4f99c8c7916a4f9b9cc3c313cb20b
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-04-28 23:14:15 +0200

    change in output: matched values, add map, + functions behind; code structure changes

--- myapp/05_shinyapp.R
+++ myapp/05_shinyapp.R
@@ -4,6 +4,11 @@ library(jsonlite)
 library(plyr)
 library(spotifyr)
 library(tidyverse)
+library(rdist)
+library(geosphere)
+library(shinythemes)
+library(leaflet)
+
 
 keys <- read_csv("../capstone/data/keys.csv") %>% 
   data.frame()
@@ -24,7 +29,7 @@ access_token <- get_spotify_access_token()
 # Google keys
 api_key <- keys$api_key[4]
 Sys.setenv(GU_API_KEY = api_key) 
-
+origin <- "Frohburgstrasse 3, 6002 Lucerne, Switzerland"
 
 
 #### UI ----
@@ -37,6 +42,8 @@ ui <- fluidPage(
     ),
     mainPanel(
       textOutput("playlist_length"),
+      textOutput("matched"),
+      leafletOutput("map"),
       dataTableOutput("restaurant_times")
     )
   )
@@ -47,7 +54,38 @@ server <- function(input, output, session) {
   
   # Function to get user's Spotify playlists
 #  playlists <- get_user_playlists(user_id, limit = 50)
-  
+
+ 
+   # Event trigger for playlist selection
+   output$playlist_selector <- renderUI({
+     playlists # <- get_user_playlists(user_id, limit = 50)
+     playlist_names <- playlists$name
+     selectInput("selected_playlist", "Select a playlist:", choices = playlists$name)
+   })
+   
+   
+   # Event trigger for playlist length calculation
+   playlist_length <- reactive({
+     # Find the selected playlist and get its ID
+     selected_name <- input$selected_playlist
+     selected_id <- ""
+     for (i in seq_along(playlists)) {
+       if (playlists[[i]]$name == selected_name) {
+         playlist_id <- playlists[[i]]$id
+         break
+       }
+     }
+     #playlists
+     playlist_length <- get_playlist_length(playlist_id, access_token)
+     return(playlist_length)
+   })
+  # # Event trigger for playlist length calculation
+  # playlist_length <- reactive({
+  #   playlist_id <- playlists[grep(input$selected_playlist, sapply(playlists, function(p) p$name))[[1]]]$id
+  #   playlist_length <- get_playlist_length(playlist_id)
+  #   
+  #   return(playlist_length)
+  # })
   
   # Function to get the length of a Spotify playlist
   get_playlist_length <- function(playlist_id) {
@@ -58,32 +96,12 @@ server <- function(input, output, session) {
   }
  
 
-  # Event trigger for playlist selection
-  output$playlist_selector <- renderUI({
-    #playlists  <- get_user_playlists(user_id, limit = 50)
-    playlist_names <- playlists$name
-    selectInput("selected_playlist", "Select a playlist:", choices = playlist_names)
-  })
-  
-  # Event trigger for playlist length calculation
-  playlist_length <- reactive({
-    playlist_id <- NA
-    playlist_id <- playlists[grep(input$selected_playlist, sapply(playlists, function(p) p$name))[[1]]]$id
-    playlist_length <- get_playlist_length(playlist_id)
-    
-    return(playlist_length)
-  })
   
   # Output for playlist length calculation
   output$playlist_length <- renderText({
     paste0("Length of ", input$selected_playlist, " playlist: ", playlist_length(), " sec")
   })
   
-  # Event trigger for restaurant times calculation
-  restaurant_times <- reactive({
-   
-    data.frame(Restaurant = places$place_name, Route_Time = places$route_times_sec)
-  })
   
   
   # Output for restaurant times calculation
@@ -91,6 +109,55 @@ server <- function(input, output, session) {
     restaurant_times()
   })
   
+  #Calculation of time matching
+  # Calculate absolute differences
+  time_matched <- reactive({
+    # Calculate absolute differences
+     diffs <- abs(places$route_times_sec - playlist_length)
+   
+   # Find index of closest value
+     idx <- which.min(diffs)
+   
+   # Print closest value and the name of the restaurant
+       closest_val <- places$route_times_sec[idx]
+       placename <- places$place_name[idx]
+       destination <- places$place_address[idx]
+       matched <- as.character(print(c(closest_val, placename)))
+   return(matched)}) 
+  
+  # Event trigger for restaurant times calculation
+  restaurant_times <- reactive({
+    
+    data.frame(matched)
+  })
+  
+  # Output for restaurant times calculation
+  output$out <- renderText(matched)
+  
+  # Get route 
+  get_route_coordinates <- function(origin, destination, api_key) {
+    endpoint <- paste0("https://maps.googleapis.com/maps/api/directions/json",
+                       "?origin=", origin,
+                       "&destination=", destination,
+                       "&mode=walking",
+                       "&key=", api_key)
+    res <- GET(endpoint)
+    stop_for_status(res)
+    data <- fromJSON(content(res, "text"), simplifyDataFrame = TRUE)
+    route_coordinates <- data$routes[[1]]$legs[[1]]$steps$lat_lngs
+    return(route_coordinates)
+
+  }
+  
+  # Event trigger for getting route coordinates
+  observeEvent(input$submit, {
+    route_coordinates <- get_route_coordinates(input$origin, input$destination, api_key)
+    output$map <- renderLeaflet({
+      leaflet() %>%
+        addTiles() %>%
+        addPolylines(data = route_coordinates)
+    })
+  })
 }
 
 
