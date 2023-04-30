047c8561cfd21292dee0a095a0e8b27e55a2ffe2
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-04-30 14:57:56 +0200

    places to places_df

--- scripts/04_time_match.R
+++ scripts/04_time_match.R
@@ -1,12 +1,12 @@
 # Calculate absolute differences between selected playlist and each place
-diffs <- abs(places$route_times_sec - playlist_length_sec)
+diffs <- abs(places_df$route_times_sec - playlist_length_sec)
 
 # Find index of closest value
 idx <- which.min(diffs)
 
 # Save the closest value
-closest_val <- places$route_times_sec[idx]
-placename <- places$place_name[idx]
+closest_val <- places_df$route_times_sec[idx]
+placename <- places_df$place_name[idx]
 
 # Print the result
 print(c(closest_val, placename))
