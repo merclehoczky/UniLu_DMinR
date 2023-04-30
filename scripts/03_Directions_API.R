97a20f3ceff4d3ec6d5ce7b1ceeaf1599bd0f3b6
Mercédesz Lehoczky <merc.lehoczky@gmail.com>
2023-04-30 14:19:23 +0200

    Update comments

--- scripts/03_Directions_API.R
+++ scripts/03_Directions_API.R
@@ -8,7 +8,6 @@ Sys.setenv(GU_API_KEY = api_key)
 
 # Define API endpoint and API key
 api_endpoint <- "https://maps.googleapis.com/maps/api/directions/json"
-#api_key <- "YOUR_API_KEY"
 
 # Define the start location
 start_location <- "University of Lucerne, Switzerland"
